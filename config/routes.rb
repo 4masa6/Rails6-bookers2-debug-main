e03dc0efba529bdde3a7bece1309b2746efa7090
mogata <70381096+4masa6@users.noreply.github.com>
2022-02-27 16:30:49 +0900

    [update]コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index,:show,:edit,:update]
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
 
