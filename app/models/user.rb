49a5a01d302c38feb82cc8f0b0ea627f4f82bbb8
mogata <70381096+4masa6@users.noreply.github.com>
2022-02-28 22:09:13 +0900

    [update]フォロー・フォロワー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,28 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, presence: true
+
+  def following?(other_user)
+    following_relationships.find_by(following_id: other_user.id)
+  end
+
+  def follow(other_user)
+    following_relationships.create(following_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    following_relationships.find_by(following_id: other_user.id).destroy
+  end
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
